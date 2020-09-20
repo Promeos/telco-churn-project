325b998b069fc1d1363a9d58eaf80d8952e9123d
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-09-19 21:18:32 -0500

    upload files

---
+++ acquire.py
@@ -0,0 +1,35 @@
+# Acquire Telecommunications Data
+
+import os
+import pandas as pd
+import env
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    '''
+    Returns a formatted url to access a SQL database.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+    
+def get_telecom_data():
+    '''
+    Returns Telecommunication Data
+    '''
+    file = 'telecom_data.csv'
+
+    if os.path.isfile(file):
+        return pd.read_csv('telecom.csv')
+    else:
+        df = pd.read_sql("""
+                        select *
+                        from customers
+                        join internet_service_types using(internet_service_type_id)
+                        join contract_types using(contract_type_id)
+                        join payment_types using(payment_type_id);
+                        """,
+                        get_connection('telco_churn')
+                        )
+        
+        df.to_csv('telecom_data.csv', index=False)
+        return df
+
